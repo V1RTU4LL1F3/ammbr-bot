eac829593ea723f08ba760f34af6d73c37b396e1
V1RTU4LL1F3 <nicolasrazeira@hotmail.com>
2016-06-28 23:29:45 -0300

    Update bBot.js

--- bBot.js
+++ bBot.js
@@ -52,7 +52,7 @@
       scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
       roomLock: false, // Requires an extension to re-load the script
       startupCap: 1, // 1-200
-      startupVolume: 50, // 0-100
+      startupVolume: 20, // 0-100
       startupEmoji: true, // true or false
       autowoot: false,
       autoskip: false,
@@ -87,7 +87,7 @@
       motdEnabled: false,
       motdInterval: 10,
       motd: "Nenhuma mensagem foi adicionada",
-      filterChat: true,
+      filterChat: false,
       etaRestriction: false,
       welcome: true,
       opLink: null,
