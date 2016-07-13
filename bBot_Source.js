2047ea32399dec336b6ca25ae6737726f0b1ddc4
V1RTU4LL1F3 <nicolasrazeira@hotmail.com>
2016-07-13 18:13:22 -0300

    Update bBot_Source.js

--- bBot_Source.js
+++ bBot_Source.js
@@ -98,7 +98,7 @@
     var loadChat = function (cb) {
         if (!cb) cb = function () {
         };
-        $.get("https://rawgit.com/V1RTU4LL1F3/fatgasda/master/LangIndex.json", function (json) {
+        $.get("https://rawgit.com/basicBot/source/master/lang/langIndex.json", function (json) {
             var link = basicBot.chatLink;
             if (json !== null && typeof json !== "undefined") {
                 langIndex = json;
@@ -2470,7 +2470,7 @@
                         if (msg.length <= cmd.length + 1) return API.sendChat(subChat(basicBot.chat.currentlang, {language: basicBot.settings.language}));
                         var argument = msg.substring(cmd.length + 1);
 
-                        $.get("https://rawgit.com/V1RTU4LL1F3/fatgasda/master/LangIndex.json", function (json) {
+                        $.get("https://rawgit.com/basicBot/source/master/lang/langIndex.json", function (json) {
                             var langIndex = json;
                             var link = langIndex[argument.toLowerCase()];
                             if (typeof link === "undefined") {
