ec75530b2a78dd7ffb12e07324090099fbcfcb97
NiceATC <NiceATC@users.noreply.github.com>
2017-04-24 20:07:41 -0300

    Update bBot_Source_Testes.js

--- bBot_Source_Testes.js
+++ bBot_Source_Testes.js
@@ -929,7 +929,7 @@
                             name = user.username;
                             var perm = basicBot.userUtilities.getPermission(chat.uid);
                             if (perm < 2) return API.sendChat(subChat(basicBot.chat.dclookuprank, {name: chat.un}));
-                        }
+                        });
                         var user = basicBot.userUtilities.lookupUserName(name);
                         if (typeof user === 'boolean') return API.sendChat(subChat(basicBot.chat.invaliduserspecified, {name: chat.un}));
                         var toChat = basicBot.userUtilities.dclookup(user.id);
