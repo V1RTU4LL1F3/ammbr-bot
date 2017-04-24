26b5bee813c75feee1e03d8b145d484da9a16361
NiceATC <NiceATC@users.noreply.github.com>
2017-04-24 20:09:48 -0300

    Update bBot_Source_Testes.js

--- bBot_Source_Testes.js
+++ bBot_Source_Testes.js
@@ -891,57 +891,7 @@
             if (!basicBot.chatUtilities.commandCheck(chat))
                 basicBot.chatUtilities.action(chat);
         },
-        eventUserjoin: function (user) {
-            var known = false;
-            var index = null;
-            for (var i = 0; i < basicBot.room.users.length; i++) {
-                if (basicBot.room.users[i].id === user.id) {
-                    known = true;
-                    index = i;
-                }
-            }
-            var greet = true;
-            var welcomeback = null;
-            if (known) {
-                basicBot.room.users[index].inRoom = true;
-                var u = basicBot.userUtilities.lookupUser(user.id);
-                var jt = u.jointime;
-                var t = Date.now() - jt;
-                if (t < 10 * 1000) greet = false;
-                else welcomeback = true;
-            }
-            else {
-                basicBot.room.users.push(new basicBot.User(user.id, user.username));
-                welcomeback = false;
-            }
-            for (var j = 0; j < basicBot.room.users.length; j++) {
-                if (basicBot.userUtilities.getUser(basicBot.room.users[j]).id === user.id) {
-                    basicBot.userUtilities.setLastActivity(basicBot.room.users[j]);
-                    basicBot.room.users[j].jointime = Date.now();
-                }
-
-            }
-            if (basicBot.settings.welcome && greet) {
-                welcomeback ?
-                    setTimeout(function (user) {
-                        API.sendChat(subChat(basicBot.chat.welcomeback, {name: user.username}));
-                        setTimeout(function(){
-                            name = user.username;
-                            var perm = basicBot.userUtilities.getPermission(chat.uid);
-                            if (perm < 2) return API.sendChat(subChat(basicBot.chat.dclookuprank, {name: chat.un}));
-                        });
-                        var user = basicBot.userUtilities.lookupUserName(name);
-                        if (typeof user === 'boolean') return API.sendChat(subChat(basicBot.chat.invaliduserspecified, {name: chat.un}));
-                        var toChat = basicBot.userUtilities.dclookup(user.id);
-                        API.sendChat(toChat);
-                        }, 2000);
-                    }, 1 * 1000, user)
-                    :
-                    setTimeout(function (user) {
-                        API.sendChat(subChat(basicBot.chat.welcome, {name: user.username}));
-                    }, 1 * 1000, user);
-            }
-        },
+        
         eventUserleave: function (user) {
             var lastDJ = API.getHistory()[0].user.id;
             for (var i = 0; i < basicBot.room.users.length; i++) {
