bbdff80d4e3362df5b51a7ab86879e71f7caad16
NiceATC <NiceATC@users.noreply.github.com>
2017-05-17 00:37:02 -0300

    Update IA.js

--- IA.js
+++ IA.js
@@ -1,11 +1,5 @@
 API.on('chat', function(obj) {
     var user = API.getUser().id
-    if(obj.message.indexOf("teste de intenligencia") != -1){
-            API.sendChat("Parece funcionar u.u");
-        }
-});
-API.on('chat', function(obj) {
-    var user = API.getUser().id
     if(obj.message.indexOf("quais são as regras Loli ?") != -1){
             API.sendChat("nossas regras? aqui estão elas :D http://goo.gl/i7JUIX");
         }
