b1690170a0424a8b3796b096e709726b806ee7ce
V1RTU4LL1F3 <nicolasrazeira@hotmail.com>
2017-05-17 00:39:34 -0300

    Update IA.js

--- IA.js
+++ IA.js
@@ -1,6 +1,6 @@
 API.on('chat', function(obj) {
     var user = API.getUser().id
-    if(obj.message.indexOf("quais são as regras Loli ?") != -1){
+    if(obj.message.indexOf("quais são as regras Loli?", "link das regras") != -1){
             API.sendChat("nossas regras? aqui estão elas :D http://goo.gl/i7JUIX");
         }
 });
