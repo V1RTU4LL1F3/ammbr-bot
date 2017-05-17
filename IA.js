aba61d0fe8c0ba9adc79c8ff4e4d8d136dca39b3
V1RTU4LL1F3 <nicolasrazeira@hotmail.com>
2017-05-17 00:56:14 -0300

    Update IA.js

--- IA.js
+++ IA.js
@@ -1,6 +1,33 @@
 API.on('chat', function(obj) {
     var user = API.getUser().id
-    if(obj.message.indexOf("quais são as regras loli?", "link das regras") != -1){
-            API.sendChat("nossas regras? aqui estão elas :D http://goo.gl/i7JUIX");
+    if(obj.message.indexOf("quais são as regras loli?") != -1){
+            API.sendChat("nossas regras? aqui estão elas :D https://goo.gl/O5nQJ0");
+        
+    if(obj.message.indexOf("quais sao as regras loli?") != -1){
+            API.sendChat("nossas regras? aqui estão elas :D https://goo.gl/O5nQJ0");
+        
+    if(obj.message.indexOf("quais são as regras loli") != -1){
+            API.sendChat("nossas regras? aqui estão elas :D https://goo.gl/O5nQJ0");
+        
+    if(obj.message.indexOf("quais são as regras da sala?") != -1){
+            API.sendChat("nossas regras? aqui estão elas :D https://goo.gl/O5nQJ0");
+        
+    if(obj.message.indexOf("quais sao as regras da sala?") != -1){
+            API.sendChat("nossas regras? aqui estão elas :D https://goo.gl/O5nQJ0");
+        
+    if(obj.message.indexOf("quais são as regras da sala") != -1){
+            API.sendChat("nossas regras? aqui estão elas :D https://goo.gl/O5nQJ0");
+        
+    if(obj.message.indexOf("kd as regras?") != -1){
+            API.sendChat("nossas regras? aqui estão elas :D https://goo.gl/O5nQJ0");
+        
+    if(obj.message.indexOf("cadê as regras?") != -1){
+            API.sendChat("nossas regras? aqui estão elas :D https://goo.gl/O5nQJ0");
+        
+    if(obj.message.indexOf("kd as regras") != -1){
+            API.sendChat("nossas regras? aqui estão elas :D https://goo.gl/O5nQJ0");
+        
+    if(obj.message.indexOf("cadê as regras") != -1){
+            API.sendChat("nossas regras? aqui estão elas :D https://goo.gl/O5nQJ0");
         }
 });
