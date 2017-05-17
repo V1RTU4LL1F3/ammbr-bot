c06363cdf4691617e266119a3740ede41cc609b0
NiceATC <NiceATC@users.noreply.github.com>
2017-05-17 01:45:26 -0300

    Update IA.js

--- IA.js
+++ IA.js
@@ -2,7 +2,6 @@
 //REGRAS
 
 API.on('chat', function(obj) {
-    var user = API.getUser().id
     if(obj.message.indexOf("quais são as regras loli") != -1){
             API.sendChat("Nossas regras? aqui estão elas :D https://goo.gl/O5nQJ0");
         }
