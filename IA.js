8d47bcdcf0b0d5d8779f94b1ddd7a43f136ec375
NiceATC <NiceATC@users.noreply.github.com>
2017-05-19 14:40:57 -0300

    Update IA.js

--- IA.js
+++ IA.js
@@ -154,4 +154,7 @@ API.on('chat', function(obj) {
     if(obj.message.indexOf("Você fala demais amigo") != -1){
             API.sendChat("Pow pow pow pei pei");
         }
+    if(obj.message.indexOf("Lailson") != -1){
+            API.sendChat("Nem é gente... é Otaku fedido");
+        }
 });
