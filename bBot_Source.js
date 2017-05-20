737363ff66a08eb3afdbcf2d0481a6b0b759369a
NiceATC <NiceATC@users.noreply.github.com>
2017-05-20 01:41:30 -0300

    Update bBot_Source.js

--- bBot_Source.js
+++ bBot_Source.js
@@ -230,7 +230,7 @@
     var botCreatorIDs = ["3851534", "4105209", "3926149"];
 
     var basicBot = {
-        version: "3.2.4 (19/05/17)",
+        version: "3.2.5 (19/05/17)",
         status: false,
         name: "basicBot",
         loggedInID: null,
